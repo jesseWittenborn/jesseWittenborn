729199a7f4b813e9bf9d7440df1c5b6d9a979721
jesseWittenborn <99203000+jesseWittenborn@users.noreply.github.com>
2022-02-28 09:53:08 -0500

    Add files via upload

---
+++ Encrypted Message.py
@@ -0,0 +1,28 @@
+# Jesse Wittenborn
+# 2/15/2021-2/22/2022
+# Encrypted Message
+
+def main():
+    message = input("What message would you like to encrypt?")
+	
+    encodedMessage = []
+    decodedMessage = ""
+    for m in message:
+        encodedMessage.append( ord(m))
+   
+	
+    print (" Encoded Message: ")
+    print (encodedMessage)
+    for e in encodedMessage:
+                 decodedMessage = decodedMessage + chr(e)
+	 
+	
+        
+	
+    print (" Decoded Message: ")
+    print (decodedMessage)
+	 
+	 
+    for e in encodedMessage:
+	        decodedMessage + chr(e)
+main()
