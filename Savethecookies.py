ad0f1f0bb576970697230dde34ca5093b9aecca8
jesseWittenborn <99203000+jesseWittenborn@users.noreply.github.com>
2022-03-15 11:33:41 -0400

    Add files via upload

---
+++ Savethecookies.py
@@ -0,0 +1,19 @@
+#Jesse Wittenborn
+#3/15/2022
+#Ice cream distribution effort
+def main():
+  
+  #Greeting
+  
+  firstName = input("Enter your name: ")
+  print("Hello, " + firstName + "!") 
+  
+ #User's cookie combination and tips user to engage in an important world improving mission
+  flavor = input(" Enter favorite cookie:") 
+  topping = input(" Enter favorite cookie topping:")
+  word3 = flavor + topping 
+   
+  print (" Your favorite cookie is " + flavor + " and your favorite topping is " + topping + "." " Sadly not everyone is as fortunate as you are, some are not able to eat their cookie combinations, this is due to the fact that so many cookies are simply wasted! But this can all be corrected, join the Cookie Distribution effort today to ensure a sweeter tomorow!") 
+  
+main()
+
