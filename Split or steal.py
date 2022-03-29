1ce3d662c0990b472666c358b971e1b83be8b131
jesseWittenborn <99203000+jesseWittenborn@users.noreply.github.com>
2022-03-29 12:03:23 -0400

    Add files via upload

---
+++ Split or steal.py
@@ -0,0 +1,21 @@
+#Split or steal
+#3/9/2022
+#Jesse Wittenborn
+
+
+
+def main():
+    print("Welcome to split or steal!")
+    name = input("Im your host, Hank! what's your name?")
+    poorBaby = input(" Now the game is very simple choose split, or steal " + name  + "(select s1 to split, or s2 to steal)")
+    if poorBaby == "s1":
+        print("I chose steal, we both lose " + name  + " jI can't believe you'd steal from me lol well good thing im not any better!XD")
+        print("This was fun, come play again soon " + name + "! or else...")
+    if poorBaby == "s2":
+        print("I chose steal, you lose and I keep my money hahaha, thanks for being a nice guy. ")
+        print("This was fun, come play again soon " + name + "! or else...")
+   
+       
+
+
+main()
